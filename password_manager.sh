bd260d748c883cb6fec902872f09de2c3fc00313
kakinoki <kknk.courage@gmail.com>
2024-07-14 05:14:19 +0900

    1.whileを用いた操作後メニューに戻るループの記述、それに伴う一部テキストの削除 2.Get Password,Exit入力時の処理の記述 3.スクリプト内のファイル名を訂正
    1.Exitの入力があるまで操作を継続できるようにするため。
    2.Get Password入力時はパスワードの取得、Exit入力時はスクリプトを中止する機能実装のため
    3.誤字訂正

--- password_manager.sh
+++ password_manager.sh
@@ -1,45 +1,51 @@
 #!/bin/bash
 #起動時
 echo "パスワードマネージャーへようこそ！"
-echo "次の選択肢から入力してください(Add Password/Get Password/Exit)："
+while true; do
+ echo "次の選択肢から入力してください(Add Password/Get Password/Exit)："
 #選択肢による処理分岐
-read choices
+ read choices
 # Add Password が入力された場合
-case $choices in
- "Add Password")
+ case $choices in
+  "Add Password")
 #サービス名登録 
- echo "サービス名を入力してください："
- read service_name
+  echo "サービス名を入力してください："
+  read service_name
 #ユーザー名登録
- echo "ユーザー名を入力してください："
- read user_name
+  echo "ユーザー名を入力してください："
+  read user_name
 #パスワード登録
- echo "パスワードを入力してください："
- read password
+  echo "パスワードを入力してください："
+  read password
 #入力完了
- echo "$service_name:$user_name:$password" >> password-store.txt
- echo "パスワードの追加は成功しました。"
- echo "次の選択肢から入力してください(Add Password/Get Password/Exit)："
- ;;
-# Get Password が入力された場合
- "Get Password")
- echo "サービス名を入力してください："
-## サービス名が保存されていなかった場合
- echo "そのサービスは登録されていません。"
-## サービス名が保存されていた場合 
- echo "サービス名："
- echo "ユーザー名："
- echo "パスワード："
- echo "次の選択肢から入力してください(Add Password/Get Password/Exit)："
- ;;
-# Exit が入力された場合
- "Exit")
-  echo "Thank you!"
+  echo "$service_name:$user_name:$password" >> password_store.txt
+  echo "パスワードの追加は成功しました。"
   ;;
+# Get Password が入力された場合
+  "Get Password")
+  echo "サービス名を入力してください："
+  read service_name
+  password=$(grep "^$service_name:" password_store.txt | cut -d: -f3)
+   if [ -z "$password" ]; then
+## サービス名が保存されていなかった場合
+    echo "そのサービスは登録されていません。"
+## サービス名が保存されていた場合
+   else
+    user_name=$(grep "^$service_name:" password_store.txt | cut -d: -f2)
+    echo "サービス名：$service_name"
+    echo "ユーザー名：$user_name"
+    echo "パスワード：$password"
+    fi
+    ;;
+# Exit が入力された場合
+  "Exit")
+   echo "Thank you!"
+   break
+   ;;
 ## プログラムが終了
-
 # Add Password/Get Password/Exit 以外が入力された場合
- *)
- echo "入力が間違えています。Add Password/Get Password/Exit から入力してください。"
- ;;
-esac
+   *)
+   echo "入力が間違えています。Add Password/Get Password/Exit から入力してください。"
+   ;;
+ esac
+done
